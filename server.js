ece4750ce7bfa9b42acc111942a261d71525988a
richoulondon-wq <richoulondon@gmail.com>
2026-03-10 00:34:16 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+  cors: { origin: "*", methods: ["GET","POST"] }
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+let waitingUsers = [];
+
+io.on("connection", socket => {
+  console.log("User connected");
+
+  socket.on("join", data => {
+    socket.userInfo = data;
+    console.log(`${data.name} joined`);
+  });
+
+  socket.on("find", () => {
+    waitingUsers = waitingUsers.filter(s => s !== socket);
+    if(waitingUsers.length > 0){
+      const partner = waitingUsers.shift();
+      socket.partner = partner;
+      partner.partner = socket;
+      socket.emit("matched");
+      partner.emit("matched");
+    } else {
+      waitingUsers.push(socket);
+      socket.emit("status","Searching for a partner...");
+    }
+  });
+
+  socket.on("next", () => {
+    if(socket.partner){
+      socket.partner.emit("partner-left");
+      socket.partner.partner = null;
+      socket.partner = null;
+    }
+    socket.emit("status","Searching for a new partner...");
+    socket.emit("find");
+  });
+
+  socket.on("message", msg => { if(socket.partner) socket.partner.emit("message", msg); });
+  socket.on("image", data => { if(socket.partner) socket.partner.emit("image", data); });
+  socket.on("offer", data => { if(socket.partner) socket.partner.emit("offer", data); });
+  socket.on("answer", data => { if(socket.partner) socket.partner.emit("answer", data); });
+  socket.on("ice", data => { if(socket.partner) socket.partner.emit("ice", data); });
+
+  socket.on("disconnect", () => {
+    waitingUsers = waitingUsers.filter(s => s !== socket);
+    if(socket.partner){
+      socket.partner.emit("partner-left");
+      socket.partner.partner = null;
+    }
+  });
+});
+
+const PORT = process.env.PORT || 10000;
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
