66495d8778d04e4836cb75d8dfd516237ffc6cca
richoulondon-wq <richoulondon@gmail.com>
2026-03-10 01:07:38 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http, {
-  cors: { origin: "*", methods: ["GET","POST"] }
-});
+const io = require("socket.io")(http, { cors: { origin: "*" } });
 
 app.use(express.static(path.join(__dirname, "public")));
 
